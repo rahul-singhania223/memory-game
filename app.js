730f508d706515b06549c91da2ad76374496cbe9
RA.one <rahulsinghania406@gmail.com>
2023-03-14 07:33:33 +0530

    project setup

---
+++ app.js
@@ -0,0 +1,54 @@
+const cardArr = [
+    {
+        name: "img1",
+        src: "/assets/image-1.png"
+    },
+    {
+        name: "img2",
+        src: "/assets/image-2.png"
+    },
+    {
+        name: "img3",
+        src: "/assets/image-3.png"
+    },
+    {
+        name: "img4",
+        src: "/assets/image-4.png"
+    },
+    {
+        name: "img5",
+        src: "/assets/image-5.png"
+    },
+    {
+        name: "img6",
+        src: "/assets/image-6.png"
+    },
+    {
+        name: "img1",
+        src: "/assets/image-1.png"
+    },
+    {
+        name: "img2",
+        src: "/assets/image-2.png"
+    },
+    {
+        name: "img3",
+        src: "/assets/image-3.png"
+    },
+    {
+        name: "img4",
+        src: "/assets/image-4.png"
+    },
+    {
+        name: "img5",
+        src: "/assets/image-5.png"
+    },
+    {
+        name: "img6",
+        src: "/assets/image-6.png"
+    }
+]
+
+
+
+
